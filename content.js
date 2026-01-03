c81bc8066d926c81f8bf2893f8666a33dacf21b6
daman599 <damanpreetk117@gmail.com>
2026-01-03 21:55:44 +0530

    main features are done

--- content.js
+++ content.js
@@ -1,19 +1,30 @@
 (function () {
-    const overlay = document.createElement("div");
-    overlay.id = "blurtube-overlay";
-
     const header = document.querySelector("ytd-masthead");
     if (header) {
         header.style.position = "relative";
         header.style.zIndex = 10000;
     }
 
-    document.body.appendChild(overlay);
-    const input = document.querySelector('input[name="search_query"]');
+    function addBlur() {
+        if (document.getElementById("blurtube-overlay")) { return }
 
-    input.addEventListener("keydown", (e) => {
-        if (e.key === "Enter") {
-            document.getElementById("blurtube-overlay")?.remove();
+        const overlay = document.createElement("div");
+        overlay.id = "blurtube-overlay";
+
+        document.body.appendChild(overlay);
+    }
+
+    function removeBlur() {
+        document.getElementById("blurtube-overlay")?.remove();
+    }
+
+    function checkRoute() {
+        if (location.pathname === "/" || location.pathname.startsWith("/shorts/")) {
+            addBlur();
+        } else {
+            removeBlur();
         }
-    })
+    }
+
+    setInterval(checkRoute, 500);
 }());
